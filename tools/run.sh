b17ae1c07abd68543f6fc48af9fc8f6415f5914b
Huberto Kaiser Filho <hubertokf@gmail.com>
2020-10-28 13:36:07 -0300

    Update run.sh

--- tools/run.sh
+++ tools/run.sh
@@ -2,4 +2,4 @@
 
 label-studio start ${PROJECT_NAME:-my_project} --init -b \
              --host ${HOST:-0.0.0.0} --port ${PORT:-8080} --protocol ${PROTOCOL:-http://} \
-             --username ${USERNAME:-""} --password ${PASSWORD:-""}
+             --username ${USERNAME:-""} --password ${PASSWORD:-""} ${PARAMETERS:-""}
